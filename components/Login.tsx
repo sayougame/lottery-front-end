286526d09ce93503112adf9890f27b1829e888e7
Marcus Larsson <sayougame@gmail.com>
2023-08-29 20:46:38 +0200

    commit from home

--- components/Login.tsx
+++ components/Login.tsx
@@ -1,15 +1,31 @@
 import React from "react";
+import {
+  ThirdwebProvider,
+  ConnectWallet,
+  metamaskWallet,
+  coinbaseWallet,
+  walletConnect,
+  localWallet,
+  paperWallet,
+  useMetamask
+} from "@thirdweb-dev/react";
 
 function Login() {
+  const connectWithMetamask = useMetamask();
   return (
-    <div>
-      <div>
+    <div className="bg-[#091B18] min-h-screen flex flex-col items-center justify-center text-center">
+      <div className="flex flex-col items-center mb-10">
         <img
-          className="rounded-full h-56 wo-56 mb-10"
+          className="rounded-full h-56 w-56 mb-10"
           src="https://i.imgur.com/mMtL8sO.png"
           alt=""
         />
+        <h1 className="text-6xl text-white font-bold">Big Pig Lottery</h1>
+        <h2 className="text-white m-2">Get started by logging in with your Metamask</h2>
+        <button onClick={connectWithMetamask}
+        className="bg-white px-8 py-5 mt-10 rounded-lg shadow-lg font-bold">Login with Metamask</button>
       </div>
+      
     </div>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -11,9 +11,16 @@ import { NextPage } from "next";
 import Header from "../components/Header";
 import "tailwindcss/tailwind.css";
 import Login from "../components/Login";
+import Loading from "../components/Loading";
+
+
 
 const Home: NextPage = () => {
   const address = useAddress();
+  const {contract, isLoading} = useContract(process.env.NEXT_PUBLIC_LOTTERY_CONTRACT_ADDRESS)
+
+  
+  if (isLoading) return <Loading />
 
   if (!address) return <Login />;
   return (
@@ -26,6 +33,45 @@ const Home: NextPage = () => {
       <div>
         <Header />
       </div>
+      // Next draw
+      <div className="space-y-5 md:space-y-0 md:flex md:flex-row items-start justify-center md:space-x-5">
+        <div className="stats-container">
+        <h1 className="text-5xl text-white font-semibold text-center">The Next Draw</h1>
+        
+        <div className="flex justify-between p-2 space-x-2">
+          <div className="stats">
+            <h2 className=" text-sm">Total Pool</h2>
+          <p className="text-xl ">0.1 MATIC</p>
+          </div>
+          <div className="stats">
+            <h2 className="text-sm">Tickets Remaining</h2>
+            <p className="text-xl">100 Tickets</p>
+          </div>
+        </div>
+        
+      <div className="stats-container space-y-2">
+        <div>
+          <div className="stats">
+            <div className="flex justify-between items-center text-white pb-2">
+              <h2>Price per ticket</h2>
+              <p>0.01 MATIC</p>
+            </div>
+            <div className="flex text-white items-center space-x-2 bg-black p-4">
+              <p>TICKETS</p>
+              <input className="flex w-full bg-transparent text-right outline-none" type="number" />
+            </div>
+          </div>
+        </div>
+      </div>
+        </div>
+        </div>
+        
+        // PRice per Tickets
+        <div>
+          <div>
+
+          </div>
+        </div>
     </main>
   );
 };
