f96261d40967f36d7dd8328c44ffb2f88c92c52a
sayougame <sayougame@gmail.com>
2023-09-01 11:17:52 +0200

    nu måste de väl för fan fungera

--- components/Login.tsx
+++ components/Login.tsx
@@ -1,14 +1,5 @@
 import React from "react";
-import {
-  ThirdwebProvider,
-  ConnectWallet,
-  metamaskWallet,
-  coinbaseWallet,
-  walletConnect,
-  localWallet,
-  paperWallet,
-  useMetamask
-} from "@thirdweb-dev/react";
+import { useMetamask } from "@thirdweb-dev/react";
 
 function Login() {
   const connectWithMetamask = useMetamask();
@@ -21,11 +12,16 @@ function Login() {
           alt=""
         />
         <h1 className="text-6xl text-white font-bold">Big Pig Lottery</h1>
-        <h2 className="text-white m-2">Get started by logging in with your Metamask</h2>
-        <button onClick={connectWithMetamask}
-        className="bg-white px-8 py-5 mt-10 rounded-lg shadow-lg font-bold">Login with Metamask</button>
+        <h2 className="text-white m-2">
+          Get started by logging in with your Metamask
+        </h2>
+        <button
+          onClick={() => connectWithMetamask()}
+          className="bg-white px-8 py-5 mt-10 rounded-lg shadow-lg font-bold"
+        >
+          Login with Metamask
+        </button>
       </div>
-      
     </div>
   );
 }
